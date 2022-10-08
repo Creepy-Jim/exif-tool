14dc4ed3ad483b281f1dfa4b42a5fbddf184a533
Jim Huang <huangqi416@163.com>
2022-10-08 23:39:40 +0100

    Added exif process

--- exif-tool.py
+++ exif-tool.py
@@ -1,19 +1,36 @@
 from cgitb import handler
-from exiftool import ExifToolHelper as exif
+from exiftool import ExifToolHelper
 import argparse
+import os
 
-parser = argparse.ArgumentParser(description='Use this tool to \
-                                process image exif information.')
-# for positional arguments.
-parser.add_argument('Image Path', metavar='Path', type=str,
-                    nargs='?', help='Path of the single image', default=None)
-# for options.
-parser.add_argument('-i', '--info', action='store',
-                    help='List selected info. * for all info.')
-parser.add_argument('-a', '--add_info', action='store',
-                    help='Add selected info.')
-parser.add_argument('-d', '--del_info', action='store',
-                    help='Remove selected info.')
+def get_args():
+    parser = argparse.ArgumentParser(description='Use this tool to \
+                                    process image exif information.')
+    # for positional arguments.
+    parser.add_argument('Image Path', metavar='Path', type=str,
+                        help='Path of the single image')
+    # for options.
+    parser.add_argument('-i', '--info', action='store',
+                        help='List selected info. * for all info.')
+    parser.add_argument('-a', '--add_info', action='store',
+                        help='Add selected info.')
+    parser.add_argument('-d', '--del_info', action='store',
+                        help='Remove selected info.')
+    parsed_args = vars(parser.parse_args())
+    return parsed_args
 
-parsed_args = parser.parse_args()
-print(parsed_args)
+
+def exif_process(args):
+    try:
+        with ExifToolHelper() as exif:
+            metadata = exif.get_metadata([args["Image Path"]])
+            print(metadata)
+    except IOError:
+        print(os.getcwd())
+        print("The file is not reachable.")
+
+
+if __name__ == "__main__":
+    parsed_args = get_args()
+    print(parsed_args)
+    exif_process(parsed_args)
